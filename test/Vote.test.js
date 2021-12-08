1c5e93fcb6ab72559c556d50df0c3ccd6a3756b4
komus-Israel <komolehinIsrael@gmail.com>
2021-12-08 15:18:04 +0100

    started the unit test for electorate registration

--- test/Vote.test.js
+++ test/Vote.test.js
@@ -5,9 +5,53 @@ require('chai')
 
 
 contract('Vote', ([user1, user2, user3])=>{
+
+    let vote
    
     beforeEach(async ()=>{
-        console.log('working')
+        vote = await Vote.new()
+    })
+
+    describe("registration of electorates", ()=>{
+
+        //  initialize the details of the electorate to be registered
+
+        const firstName = 'Israel';
+        const lastName = 'Timilehin';
+        const middlename = 'Tope';
+        const state = 'Oyo';
+        const nin = '1245454';
+        const address = 'user1';
+        
+
+        it("was successful", async()=>{
+
+            // register user1
+
+            let register
+            beforeEach(async()=>{
+                const register = await vote.registerVoter(
+                    firstName,
+                    lastName,
+                    middlename,
+                    state,
+                    nin,
+    
+                    { from: user1 }
+                )
+            })
+           
+
+            console.log(register)
+
+            //  test the name of the event for this function call
+            register.event.should.be.equal('Registered')
+        })
+
+        it("failed", async()=>{
+
+        })
+
     })
 
 })
